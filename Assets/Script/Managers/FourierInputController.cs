5f45b1329a5e91687a68d24efff0703ce2f09095
朱志海 <tothesong@gmail.com>
2018-05-04 04:23:25 +0800

    rotate

--- Assets/Script/Managers/FourierInputController.cs
+++ Assets/Script/Managers/FourierInputController.cs
@@ -3,46 +3,57 @@ using UnityEngine;
 
 public class FourierInputController : MonoBehaviour {
 
-    private const float rotateDelta = 10f;
-    private const float subtractDelta = 1.5f;
-    private const float pushDelta = 1f;
-
-    private float[] marks;
+    private const float rotateSwipe = 10f;
+    private const float subtractSwipe = 1.5f;
+    private const float pushSwipe = 1f;
 
     // 设备对微小操作的“不响应区域”的大小（半径）
     private const float deadZoneSize = 10f;
 
     // 划动开始的位置
-    private Vector2 prevPos;
+    private float prevX = -10f;
+    Transform papersParentTransform;
+    private int prevIntervalId = 0;
     // 是否正在划动（划动是否已经开始）
     private bool isSwiping = false;
     // 划动仍在 deadZone 范畴内
     private bool inDeadZone = true;
-    private float totalDeltaX = 0f;
-    private int waveCount = 3;
+    private float totalSwipeX = 0f;
+    private float newSwipeX;
+    private int waveAttributesCount = 3;
+    private float leftSwipeX;
+    private float rightSwipeX;
+
+    // 区间的左右边界
+    private float[] left, right;
 
     enum OnePointPhase { Unassigned, Began, Ended }
 
     internal void SetDatas(
-        PaperData[] papersData,
-        WaveData waveData,
-        WaveController waveController
+        int waveAttributesCount,
+        Transform papersParentTransform
     ) {
-        waveCount = papersData[0].waveAttributes.Length;
+        this.waveAttributesCount = waveAttributesCount;
+        this.papersParentTransform = papersParentTransform;
     }
 
     private void Awake() {
+        left = new float[waveAttributesCount * 2 + 2];
+        right = new float[waveAttributesCount * 2 + 2];
 
-        marks = new float[waveCount * 2 + 3];
-        marks[0] = 0;
-        marks[1] = rotateDelta;
+        left[0] = 0;
+        right[0] = rotateSwipe;
 
-        for (int i = 1; i < 4; ++i) {
-            marks[i * 2] = marks[i * 2 - 1] + subtractDelta;
-            marks[i * 2 + 1] = marks[i * 2] + pushDelta;
+        for (int i = 1; i < waveAttributesCount + 1; ++i) {
+            int first = i * 2 - 1, second = i * 2;
+            left[first] = right[first - 1];
+            right[first] = left[second] = left[first] + subtractSwipe;
+            right[second] = left[second] + pushSwipe;
         }
 
-        marks[8] = marks[7] + rotateDelta;
+        int last = waveAttributesCount * 2 + 1;
+        left[last] = right[last - 1];
+        right[last] = left[last] + rotateSwipe;
     }
 
     private void Update() {
@@ -81,8 +92,8 @@ public class FourierInputController : MonoBehaviour {
 
         // 仍在划动
         if (isSwiping && isTouching) {
-            Swipe(prevPos, onePointPos);
-            prevPos = onePointPos;
+            Swipe(onePointPos);
+            prevX = onePointPos.x;
         }
 
         // 终止划动
@@ -95,38 +106,112 @@ public class FourierInputController : MonoBehaviour {
 
         // 开始划动
         if (isTouching && onePointPhase == OnePointPhase.Began) {
-            prevPos = onePointPos;
+            prevX = onePointPos.x;
             isSwiping = true;
             inDeadZone = true;
         }
     }
 
     // 处理划动操作
-    private void Swipe(Vector2 startPos, Vector2 onePointPos) {
+    private void Swipe(Vector2 onePointPos) {
         // 计算划动总位移矢量
-        Vector2 diff = onePointPos - startPos;
+        float diff = onePointPos.x - prevX;
 
-        // // 如果上一帧仍在 deadZone 内而这一帧移出了
-        // if (inDeadZone && diff.magnitude > deadZoneSize / 2) {
-        //     // 判断此次划动是在修改 A 还是 Phi
-        //     isChangingANotPhi = Mathf.Abs(diff.y) > Mathf.Abs(diff.x);
+        newSwipeX = totalSwipeX + diff;
 
-        //     // 标记此次 touch 已经脱离 deadZone
-        //     inDeadZone = false;
-        // }
+        int step = diff <= 0 ? -1 : 1;
 
-        // // 如果已经不在 deadZone 内
-        // if (!inDeadZone) {
-        //     // 要修改 A 还是 Phi
-        //     if (isChangingANotPhi) {
-        //         waveModification.A = waveModificationOrigin.A *
-        //             (diff.y * aZoomSpeed + 1);
-        //     } else {
-        //         waveModification.Phi = waveModificationOrigin.Phi +
-        //             diff.x * phiTransSpeed;
+        while (CheckInterval(prevIntervalId + step)) {
+            prevIntervalId += step;
+        }
+
+        // if (newSwipeX == marks[nextMarkId])
+        //     newSwipeX += 0.01f * (direction == 0 ? -1 : 1);
+
+        // leftSwipeX = Mathf.Min(totalSwipeX, newSwipeX);
+        // rightSwipeX = Mathf.Max(totalSwipeX, newSwipeX);
+
+        // while (leftSwipeX < marks[nextMarkId] && marks[nextMarkId] < rightSwipeX) {
+        //     if (nextMarkId == 0) {
+        //         leftRotate(1);
         //     }
-        //     // 立刻刷新纸片上波形
-        //     RefreshPapers();
+        //     if (nextMarkId == waveCount * 2 + 1) {
+        //         rightRotate(1);
+        //         newSwipeX += 1f;
+        //     }
         // }
     }
+
+    private bool CheckInterval(int intervalId) {
+        if (right[intervalId] < leftSwipeX ||
+            rightSwipeX < left[intervalId]) {
+            return false;
+        }
+
+        LerpInterval(intervalId);
+
+        return true;
+    }
+
+    private void LerpInterval(int intervalId) {
+        float inverseLerp = Mathf.InverseLerp(left[intervalId], right[intervalId], newSwipeX);
+        if (intervalId == 0) {
+            firstRotate(inverseLerp);
+            return;
+        }
+        if (intervalId == waveAttributesCount * 2 + 1) {
+            lastRotate(inverseLerp);
+            return;
+        }
+
+        int waveId = (intervalId - 1) / 2;
+        if (intervalId % 1 == 1) {
+            Subtract(waveId, inverseLerp);
+        } else {
+            Push(waveId, inverseLerp);
+        }
+    }
+
+    private void firstRotate(float inverseLerp) {
+        papersParentTransform.rotation = Quaternion.identity;
+        papersParentTransform.Rotate(Vector3.right * Mathf.Lerp(0f, -30f, inverseLerp));
+        papersParentTransform.Rotate(Vector3.up * Mathf.Lerp(0f, 45f, inverseLerp));
+    }
+
+    private void lastRotate(float inverseLerp) {
+        papersParentTransform.rotation = Quaternion.Euler(Vector3.up * 90);
+        papersParentTransform.Rotate(Vector3.right * Mathf.Lerp(0f, -30f, 1 - inverseLerp));
+        papersParentTransform.Rotate(Vector3.up * Mathf.Lerp(0f, -45f, 1 - inverseLerp));
+    }
+
+    private void Subtract(int waveId, float inverseLerp) {
+        throw new NotImplementedException();
+    }
+
+    private void Push(int waveId, float inverseLerp) {
+        throw new NotImplementedException();
+    }
+
+    // // 如果上一帧仍在 deadZone 内而这一帧移出了
+    // if (inDeadZone && diff.magnitude > deadZoneSize / 2) {
+    //     // 判断此次划动是在修改 A 还是 Phi
+    //     isChangingANotPhi = Mathf.Abs(diff.y) > Mathf.Abs(diff.x);
+
+    //     // 标记此次 touch 已经脱离 deadZone
+    //     inDeadZone = false;
+    // }
+
+    // // 如果已经不在 deadZone 内
+    // if (!inDeadZone) {
+    //     // 要修改 A 还是 Phi
+    //     if (isChangingANotPhi) {
+    //         waveModification.A = waveModificationOrigin.A *
+    //             (diff.y * aZoomSpeed + 1);
+    //     } else {
+    //         waveModification.Phi = waveModificationOrigin.Phi +
+    //             diff.x * phiTransSpeed;
+    //     }
+    //     // 立刻刷新纸片上波形
+    //     RefreshPapers();
+    // }
 }
