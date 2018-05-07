4e3e52f89231e7093d56bbce1b240e99227ccaf1
朱志海 <tothesong@gmail.com>
2018-05-08 02:08:56 +0800

    Change LevelData to include multiple papers mod

--- Assets/Script/Datas/LevelData.cs
+++ Assets/Script/Datas/LevelData.cs
@@ -6,5 +6,5 @@ public class LevelData {
     /// <summary> 关卡内的多个纸片数据对象 </summary>
     public PaperData[] papersData;
     /// <summary> 用户通关需要做的修改 </summary>
-    public WaveModification modification;
+    public WaveModification[] modifications;
 }
