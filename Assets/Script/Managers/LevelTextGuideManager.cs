bd85670bc8d12ed663074e0c407da73277edf00d
LAndyPommel <landypommel@gmail.com>
2018-05-08 19:56:54 +0800

    Add levelTextguide
    TODO:fix bug for only appear once

---
+++ Assets/Script/Managers/LevelTextGuideManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTextGuideManager : MonoBehaviour {
+
+	public TextsFade[] textsFades;
+	// Use this for initialization
+	public void ShowTextsByLevelIndex()
+	{
+		Debug.Log(GameManager.Instance.GetTotalMissionIndex());
+		textsFades[GameManager.Instance.GetTotalMissionIndex()-1].ShowTexts();
+	}
+}
---
+++ Assets/Script/UI & Sprite/TextsFade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TextsFade : MonoBehaviour {
+
+	private Text[] texts;
+	private Tweener[] textTweens;
+	private Tweener[] textRTweens;
+	// Use this for initialization
+	void Awake () {
+		texts = GetComponentsInChildren<Text>();
+		textTweens = new Tweener[texts.Length];
+		textRTweens = new Tweener[texts.Length];
+		Debug.Log(texts.Length);
+	}
+	void Start(){
+		for (int i = 0; i < texts.Length; i++)
+		{
+			textTweens[i] = texts[i].DOFade(1,0.6f).Pause().SetAutoKill(false).OnComplete( ()=> {textTweens[i].Rewind();});
+			textRTweens[i] = texts[i].DOFade(0,0.2f).Pause().SetAutoKill(false).OnComplete( ()=> {textRTweens[i].Rewind();});
+			Debug.Log("complete " + i);
+		}
+	}
+	
+	// Update is called once per frame
+	public void ShowTexts()
+	{
+		foreach (Tweener tw in textTweens)
+		{
+			tw.Play();
+		}
+		Invoke("HideTexts",10f);
+	} 
+	public void HideTexts()
+	{
+		foreach (Tweener tw in textRTweens)
+		{
+			tw.Play();
+		}
+	} 
+}
