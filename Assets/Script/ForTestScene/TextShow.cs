c774b35563ac84016365feb8e1c98c1259d0ba7a
LAndyPommel <landypommel@gmail.com>
2018-06-06 20:52:00 +0800

    Text Show

---
+++ Assets/Script/ForTestScene/TextShow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TextShow : MonoBehaviour {
+
+
+	public Text instruct;
+	// Use this for initialization
+	void Start () {
+		Tween t = instruct.DOFade(1,0.4f).SetAutoKill(true).OnComplete(Disapear);
+	}
+	
+	// Update is called once per frame
+	void Disapear () {
+		Invoke("Reverse",5f);
+	}
+	void Reverse()
+	{
+		Tween t = instruct.DOFade(0,0.4f).SetAutoKill(true);
+	}
+}
